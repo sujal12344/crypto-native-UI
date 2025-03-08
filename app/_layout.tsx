b1e639975183ab4ec3ee97ada7d752b51e035a6b
sujal <sujalkesharwani220@gmail.com>
2025-03-08 22:06:27 +0530

    remove headerShown

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,11 +31,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
